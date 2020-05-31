f24d6ae40a1db04f77a5ffdd4776e49d60af0400
GoodPro712 <charliebrannan154@gmail.com>
2020-05-31 11:04:31 +1000

    1.4

--- FlightTimer.cs
+++ FlightTimer.cs
@@ -8,18 +8,17 @@ namespace FlightTimer
 {
 	public class FlightTimer : Mod
 	{
-		// GoodProLib v1.0 - layers.NewInterfaceLayer
 		public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
 		{
 			int mouseText = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+
 			if (mouseText != -1)
 			{
-				layers.Insert(mouseText, new LegacyGameInterfaceLayer("FlightTimer: Player Name", delegate
+				layers.Insert(mouseText, new LegacyGameInterfaceLayer("FlightTimer: Flight Timer", delegate
 				{
 					if (!Main.gameMenu && Main.LocalPlayer.wingTimeMax != Main.LocalPlayer.wingTime && ModContent.GetInstance<Config>().FlightTimer && !Main.LocalPlayer.dead)
 					{
 						string text = string.Format("{0:f" + ModContent.GetInstance<Config>().Decimal + "}", Main.LocalPlayer.wingTime / 60f);
-
 						Vector2 position = ModContent.GetInstance<Config>().Position * new Vector2(Main.screenWidth, Main.screenHeight);
 
 						Utils.DrawBorderString(Main.spriteBatch, text, position, Color.WhiteSmoke);
